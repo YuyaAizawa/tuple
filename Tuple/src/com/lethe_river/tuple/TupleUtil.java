23e9537d20f8ace26524fec8a743218f2117cac9
YuyaAizawa <selfish.curiosity@gmail.com>
2015-03-05 13:12:21 +0900

    Update TupleUtil#product.

--- Tuple/src/com/lethe_river/tuple/TupleUtil.java
+++ Tuple/src/com/lethe_river/tuple/TupleUtil.java
@@ -1,14 +1,12 @@
 package com.lethe_river.tuple;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Spliterator;
 import java.util.Spliterators;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -188,26 +186,12 @@ public class TupleUtil {
 	 * @param s2
 	 * @return 直積のTupleを要素とするStream
 	 */
+	
 	public static <T1, T2> Stream<Tuple2<T1, T2>> product(Stream<T1> s1, Stream<T2> s2) {
-		final Spliterator<T1> sp1 = s1.spliterator();
-		final Spliterator<T2> sp2 = s2.spliterator();
-		final int c1 = sp1.characteristics();
-		final int c2 = sp2.characteristics();
-		final int c = productCharacteristics(c1, c2);
-		final Iterator<Tuple2<T1, T2>> i = new Tuple2Productor<T1, T2>(
-				Spliterators.iterator(sp1),
-				Spliterators.iterator(sp2)
-				);
-		
-		if((c & Spliterator.SIZED) != 0) {
-			long limit = sp1.getExactSizeIfKnown() * sp2.getExactSizeIfKnown();
-			assert(limit < 0);
-			return StreamSupport.stream(Spliterators.spliterator(i, limit, c), false);
-		}
-		
-		return StreamSupport.stream(Spliterators.spliteratorUnknownSize(i, c), false);
+		List<T2> l2 = s2.collect(Collectors.toList());
+		return s1.flatMap(v1 -> l2.stream().map(v2 -> newTuple(v1, v2)));
 	}
-
+	
 	private static int zipCharacteristics(int... c) {
 		int dis = Arrays.stream(c).reduce((l, r) -> l | r).getAsInt();
 		int con = Arrays.stream(c).reduce((l, r) -> l & r).getAsInt();
@@ -326,60 +310,6 @@ public class TupleUtil {
 							i3.next(),
 							i4.next(),
 							i5.next());
-		}
-	}
-
-	private static int productCharacteristics(int... c) {
-		int con = Arrays.stream(c).reduce((l, r) -> l & r).getAsInt();
-		return (Spliterator.CONCURRENT & (con))
-			 | (Spliterator.DISTINCT   & (con))
-			 | (Spliterator.IMMUTABLE  & (con))
-			 |  Spliterator.NONNULL
-			 | (Spliterator.ORDERED    & (con))
-			 | (Spliterator.SIZED      & (con));
-	}
-	
-	private static class Tuple2Productor<T1, T2> implements Iterator<Tuple2<T1, T2>> {
-		private final Iterator<T1> i1;
-		private final List<T2> l2;
-		private T1 v1;
-		private Iterator<T2> i2;
-		private boolean hasNext;
-		
-		@SuppressWarnings("serial")
-		public Tuple2Productor(Iterator<T1> i1, Iterator<T2> i2) {
-			this.l2 = Collections.unmodifiableList(
-					new ArrayList<T2>(){{
-						while(i2.hasNext())
-							add(i2.next());
-					}});
-			this.i1 = i1;
-			this.i2 = l2.iterator();
-			if(i1.hasNext() && this.i2.hasNext()) {
-				v1 = i1.next();
-				hasNext = true;
-			} else {
-				hasNext = false;
-			}
-		}
-
-		@Override
-		public boolean hasNext() {
-			return hasNext;
-		}
-
-		@Override
-		public Tuple2<T1, T2> next() {
-			if(!hasNext)
-				throw new NoSuchElementException();
-			
-			if(!i2.hasNext() && i1.hasNext()) {
-				v1 = i1.next();
-				i2 = l2.iterator();
-			}
-			final Tuple2<T1, T2> retVal = newTuple(v1, i2.next());
-			hasNext = i1.hasNext() || i2.hasNext();	
-			return retVal;
 		}
 	}
 }
